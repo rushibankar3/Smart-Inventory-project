cf50ba375399b4ac74debab3b8096675f74b3e0a
Rushi Bankar <rushibankar2103@gmail.com>
2026-02-05 13:12:50 +0530

    Forecasting edited

--- services/demand_forecasting.py
+++ services/demand_forecasting.py
@@ -1,13 +1,29 @@
 import joblib
 import pandas as pd
+import os
 
-model = joblib.load("models/model.pkl")
+MODEL_PATH = os.path.join("models", "model.pkl")
+model = joblib.load(MODEL_PATH)
 
 FEATURES = [
-    'Inventory Level','Price','Discount','Holiday/Promotion',
-    'Competitor Pricing','year','month','day','day_of_week',
-    'week_of_year','is_weekend','lag_7','lag_14','lag_30',
-    'rolling_7','rolling_14','price_diff_comp','discount_flag'
+    'Inventory Level',
+    'Price',
+    'Discount',
+    'Holiday/Promotion',
+    'Competitor Pricing',
+    'year',
+    'month',
+    'day',
+    'day_of_week',
+    'week_of_year',
+    'is_weekend',
+    'lag_7',
+    'lag_14',
+    'lag_30',
+    'rolling_7',
+    'rolling_14',
+    'price_diff_comp',
+    'discount_flag'
 ]
 
 def predict_demand(df: pd.DataFrame) -> pd.DataFrame:
